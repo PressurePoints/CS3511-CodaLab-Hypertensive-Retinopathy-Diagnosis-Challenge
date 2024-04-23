0db0eaa0b448fbea81c56949b7522a98c38b3103
PressurePoints <fancymare@sjtu.edu.cn>
2024-04-23 15:41:40 +0800

    write submitting version in transform given by gpt

--- predict.py
+++ predict.py
@@ -18,13 +18,13 @@ def main():
     num_model = "s"
 
     data_transform = transforms.Compose(
-        [transforms.Resize(img_size[num_model][1]),
-         transforms.CenterCrop(img_size[num_model][1]),
+        [transforms.Resize(384),
+         transforms.CenterCrop(224),
          transforms.ToTensor(),
-         transforms.Normalize([0.5, 0.5, 0.5], [0.5, 0.5, 0.5])])
+         transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])])
 
     # load image
-    img_path = "../tulip.jpg"
+    img_path = os.path.join('./data-raw', '1-Images', '1-Training Set', '00000ce7.png')
     assert os.path.exists(img_path), "file: '{}' dose not exist.".format(img_path)
     img = Image.open(img_path)
     plt.imshow(img)
@@ -33,15 +33,8 @@ def main():
     # expand batch dimension
     img = torch.unsqueeze(img, dim=0)
 
-    # read class_indict
-    json_path = './class_indices.json'
-    assert os.path.exists(json_path), "file: '{}' dose not exist.".format(json_path)
-
-    with open(json_path, "r") as f:
-        class_indict = json.load(f)
-
     # create model
-    model = create_model(num_classes=5).to(device)
+    model = create_model(num_classes=2).to(device)
     # load model weights
     model_weight_path = "./weights/model-29.pth"
     model.load_state_dict(torch.load(model_weight_path, map_location=device))
@@ -52,14 +45,7 @@ def main():
         predict = torch.softmax(output, dim=0)
         predict_cla = torch.argmax(predict).numpy()
 
-    print_res = "class: {}   prob: {:.3}".format(class_indict[str(predict_cla)],
-                                                 predict[predict_cla].numpy())
-    plt.title(print_res)
-    for i in range(len(predict)):
-        print("class: {:10}   prob: {:.3}".format(class_indict[str(i)],
-                                                  predict[i].numpy()))
-    plt.show()
-
+    print(predict_cla)
 
 if __name__ == '__main__':
     main()
