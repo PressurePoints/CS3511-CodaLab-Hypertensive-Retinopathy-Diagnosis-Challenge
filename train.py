92b34bb115b75696dba390856c4ee310bbabecbd
PressurePoints <fancymare@sjtu.edu.cn>
2024-04-25 13:37:50 +0800

    retrain the CLAHE, but submit weight-22--Score:0.456031746

--- train.py
+++ train.py
@@ -127,7 +127,7 @@ def main(args):
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--num_classes', type=int, default=2)
-    parser.add_argument('--epochs', type=int, default=30)
+    parser.add_argument('--epochs', type=int, default=24)
     parser.add_argument('--batch-size', type=int, default=8)
     parser.add_argument('--lr', type=float, default=0.01)
     parser.add_argument('--lrf', type=float, default=0.01)
